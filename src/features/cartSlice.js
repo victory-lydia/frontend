11f228a55a57296482fe55f02e1c8f01492d9136
Victory Excel <vickylydy@gmail.com>
2023-06-10 19:54:37 +0100

    Created an online shop

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -2,44 +2,103 @@ import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 
 const initialState = {
-    cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItem"
-    )) : [],
-    cartTotalQuantity: 0,
-    cartTotalAmount: 0,
+  cartItems: localStorage.getItem("cartItems")
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [],
+  cartTotalQuantity: 0,
+  cartTotalAmount: 0,
 };
 
 const cartSlice = createSlice({
-    name: "cart",
-    initialState,
-    reducers: {
-        addToCart(state, action) {
-            // const itemIndex = state.cartItems.findIndex(
-            //     item => item.id === action.payload.id
-            console.log(action.payload, "indexValue is ");
-            console.log(state);
-            // );
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      const itemIndex = state?.cartItems?.findIndex(
+        (i) => i?.id === action.payload.id
+      );
+      //   const itemIndex = state?.cartItems?.findIndex(
+      //     (i) => i?.id === action.payload.id
+      //   );
+      console.log(itemIndex, "cartitems ====", state.cartItems, "cartItems");
+      if (itemIndex > 0) {
+        state.cartItems[itemIndex].cartQuantity += 1;
+        toast.info(
+          `increased ${state.cartItems[itemIndex].name} product quantity`,
+          {
+            position: "bottom-left",
+          }
+        );
+      } else {
+        const tempProduct = { ...action.payload, cartQuantity: 1 };
+        state.cartItems.push(tempProduct);
+        toast.success(` ${action.payload.name} added to cart `, {
+          position: "bottom-left",
+        });
+      }
 
-            const itemIndex = state.cartItems.findIndex((i) => i.id === action.payload.id)
-            if (itemIndex > 0) {
-                state.cartItems[itemIndex].cartQuantity += 1
-                toast.info(`increased ${state.cartItems[itemIndex].name} product quantity`,
-                    {
-                        position: "bottom-left",
-                    });
-            } else {
-                const tempProduct = { ...action.payload, cartQuantity: 1 };
-                state.cartItems.push(tempProduct);
-                toast.success(` ${action.payload.name} added to cart `, {
-                    position: "bottom-left",
-                });
-            }
-
-            localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
-
-        },
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
+    removeFromCart(state, action) {
+      const nextCartItems = state.cartItems.filter(
+        (cartItem) => cartItem.id !== action.payload.id
+      );
+
+      state.cartItems = nextCartItems;
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+
+      toast.error(` ${action.payload.name} remove from cart `, {
+        position: "bottom-left",
+      });
+    },
+    decreaseCart(state, action) {
+      const itemIndex = state.cartItems.findIndex(
+        (cartItem) => cartItem.id === action.payload.id
+      );
+
+      if (state.cartItems[itemIndex].cartQuantity > 1) {
+        state.cartItems[itemIndex].cartQuantity -= 1;
+
+        toast.info(`Decreased ${action.payload.name} cart quantity `, {
+          position: "bottom-left",
+        });
+      } else if (state.cartItems[itemIndex].cartQuantity === 1) {
+        const nextCartItems = state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        );
+
+        state.cartItems = nextCartItems;
+
+        toast.error(` ${action.payload.name} remove from cart `, {
+          position: "bottom-left",
+        });
+      }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+    getTotals(state, action) {
+      let { total, quantity } = state.cartItems.reduce(
+        (cartTotal, cartItem) => {
+          const { price, cartQuantity } = cartItem;
+          const itemTotal = price * cartQuantity;
+
+          cartTotal.total += itemTotal;
+          cartTotal.quantity += cartQuantity;
+
+          return cartTotal;
+        },
+        {
+          total: 0,
+          quantity: 0,
+        }
+      );
+
+      state.cartTotalQuantity = quantity;
+      state.cartTotalAmount = total;
+    },
+  },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, decreaseCart, getTotals } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 
-import productsReducer, { productsFetch } from './features/productsSlice';
-import { productApi } from './features/productApi';
-import cartReducer from './features/cartSlice';
+import productsReducer, { productsFetch } from "./features/productsSlice";
+import { productApi } from "./features/productApi";
+import cartReducer, { getTotals } from "./features/cartSlice";
 
 const store = configureStore({
-  reducer:{
+  reducer: {
     product: productsReducer,
     cart: cartReducer,
     [productApi.reducerPath]: productApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(productApi.middleware)
+    return getDefaultMiddleware().concat(productApi.middleware);
   },
 });
 
-
 store.dispatch(productsFetch());
+store.dispatch(getTotals());
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>
 );
